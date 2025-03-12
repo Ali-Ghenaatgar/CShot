511a4346496b5b66940758ed06cc60e5c86a4c93
Ali-Ghenaatgar <bardia138544@gmail.com>
2025-03-12 11:30:12 +0330

    added targets

--- main.py
+++ main.py
@@ -8,16 +8,34 @@ class Player():
         self.image = image
         self.score = score
 
+class Target():
+    def __init__(self, x, y, image):
+        self.x = x
+        self.y = y
+        self.image = image
+
+    def render(self, screen):
+        screen.blit(self.image, (self.x, self.y))
+
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('CShot')
 
+font = pygame.font.Font(None, 36)
+
 image1 = pygame.image.load('arrow.png')
 image1 = pygame.transform.scale(image1, (25, 25))
 
+target_image = pygame.image.load('target.png')
+target_image = pygame.transform.scale(target_image, (50, 50))
+
 player1 = Player(random.randint(0, 775), random.randint(0, 575), image1, score=0)
 player2 = Player(random.randint(0, 775), random.randint(0, 575), image1, score=0)
+
+target1 = Target(random.randint(0, 750), random.randint(0, 550), target_image)
+target2 = Target(random.randint(0, 750), random.randint(0, 550), target_image)
+target3 = Target(random.randint(0, 750), random.randint(0, 550), target_image)
 
 def player(image, x, y):
     screen.blit(image, (x, y))
@@ -49,6 +67,24 @@ while running:
                 xchange2 = -0.1
             if event.key == pygame.K_RIGHT:
                 xchange2 = 0.1
+            
+            if event.key == pygame.K_TAB:
+                for target in [target1, target2, target3]:
+                    if target.x < player1.x < target.x + 50 and target.y < player1.y < target.y + 50:
+                        pygame.mixer.music.load('vay-hossein.wav')
+                        pygame.mixer.music.play()
+                        player1.score += 1
+                        target.x = random.randint(0, 750)
+                        target.y = random.randint(0, 550)
+
+            if event.key == pygame.K_RETURN:
+                for target in [target1, target2, target3]:
+                    if target.x < player2.x < target.x + 50 and target.y < player2.y < target.y + 50:
+                        player2.score += 1
+                        pygame.mixer.music.load('vay-hossein.wav')
+                        pygame.mixer.music.play()
+                        target.x = random.randint(0, 750)
+                        target.y = random.randint(0, 550)
 
         if event.type == pygame.KEYUP:
             if event.key in [pygame.K_a, pygame.K_d]:
@@ -65,8 +101,17 @@ while running:
     player2.x = max(0, min(player2.x + xchange2, 775))
     player2.y = max(0, min(player2.y + ychange2, 575))
 
+    for target_obj in [target1, target2, target3]:
+        target_obj.render(screen)
+
     player(player1.image, player1.x, player1.y)
     player(player2.image, player2.x, player2.y)
+
+    score_text1 = font.render(f"Player 1: {player1.score}", True, (0, 0, 0))
+    score_text2 = font.render(f"Player 2: {player2.score}", True, (0, 0, 0))
+
+    screen.blit(score_text1, (10, 10))
+    screen.blit(score_text2, (10, 40))
 
     pygame.display.update()
 
