f7231b2130271708e098f70c587876aad219066d
Aminshah10 <aminshahmohamad1384@gmail.com>
2025-03-23 22:01:44 +0330

    new buttons

--- main.py
+++ main.py
@@ -18,12 +18,17 @@ heading.place(x=140, y=15)
 username = Entry(frame, font=("Microsoft yaHei UI Light", 15), bg="white", fg="black", border=0, width=20)
 username.place(x=30, y=100)
 username.insert(0, "Username")
-Frame(frame, width=250, height=2, bg="black").place(x=30, y=130)
+Frame(frame, width=270, height=2, bg="black").place(x=30, y=130)
 
 password = Entry(frame, font=("Microsoft yaHei UI Light", 15), bg="white", fg="black", border=0, width=20)
 password.place(x=30, y=170)
 password.insert(0, "Password")  
-Frame(frame, width=250, height=2, bg="black").place(x=30, y=200)
+Frame(frame, width=270, height=2, bg="black").place(x=30, y=200)
+#--------------------------------------------------------------------------------
+button = Button(frame, text="Login", font=("Microsoft yaHei UI Light", 15, "bold"), bg="#57a1f8", fg="white", border=0, width=20)
+button.place(x=55, y=250)
+
+new_user_button = Label(frame, text="Don't have an account?", font=("Microsoft yaHei UI Light", 9), bg="white", fg="black").place(x=55, y=300)
 
 
 root.mainloop()  # Start the main event loop
