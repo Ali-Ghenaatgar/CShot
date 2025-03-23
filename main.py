1c21454de479f4da3169be09a2247f9bb8d6adce
Aminshah10 <aminshahmohamad1384@gmail.com>
2025-03-23 22:32:06 +0330

    removing place holders default name

--- main.py
+++ main.py
@@ -18,17 +18,37 @@ heading.place(x=140, y=15)
 username = Entry(frame, font=("Microsoft yaHei UI Light", 15), bg="white", fg="black", border=0, width=20)
 username.place(x=30, y=100)
 username.insert(0, "Username")
+def on_enter(e):
+    username.delete(0, "end")
+
+def on_leave(e):
+    if username.get() == "":
+        username.insert(0, "Username")  
+
+username.bind("<FocusIn>", on_enter)
+username.bind("<FocusOut>", on_leave)
+
 Frame(frame, width=270, height=2, bg="black").place(x=30, y=130)
 
 password = Entry(frame, font=("Microsoft yaHei UI Light", 15), bg="white", fg="black", border=0, width=20)
 password.place(x=30, y=170)
 password.insert(0, "Password")  
 Frame(frame, width=270, height=2, bg="black").place(x=30, y=200)
+def on_enter(e):
+    password.delete(0, "end")
+
+def on_leave(e):
+    if password.get() == "":
+        password.insert(0, "password")  
+        
+password.bind("<FocusIn>", on_enter)
+password.bind("<FocusOut>", on_leave)
 #--------------------------------------------------------------------------------
 button = Button(frame, text="Login", font=("Microsoft yaHei UI Light", 15, "bold"), bg="#57a1f8", fg="white", border=0, width=20)
 button.place(x=55, y=250)
 
 new_user_button = Label(frame, text="Don't have an account?", font=("Microsoft yaHei UI Light", 9), bg="white", fg="black").place(x=55, y=300)
-
+sign_up = Button (frame,width=6, text="Sign Up", fg="#57a1f8", bg="white", border=0, cursor ="hand2")
+sign_up.place(x=195, y=300)
 
 root.mainloop()  # Start the main event loop
